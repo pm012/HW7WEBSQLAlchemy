6e569c2d72879313423177db6c92b8e832416fd5
Serhii Kroshka <pm012@ukr.net>
2024-04-15 19:07:59 +0300

    Updated 2c67bfa6b665_initial_migration.py fixed falure if database structure is already exists

--- alembic/versions/2c67bfa6b665_initial_migration.py
+++ alembic/versions/2c67bfa6b665_initial_migration.py
@@ -22,81 +22,91 @@ def upgrade() -> None:
     metadata.bind = op.get_bind()
     existing_tables = metadata.tables.keys()
     # Create the 'groups' table
-    if 'groups'  not in existing_tables:
-        op.create_table(
-            'groups',
-            sa.Column('id', sa.Integer(), nullable=False),
-            sa.Column('group_code', sa.String(length=50), nullable=True),
-            sa.Column('group_name', sa.String(length=100), nullable=True),
-            sa.PrimaryKeyConstraint('id')
-        )
+    if 'groups' not in existing_tables:
+        with op.batch_alter_table('groups') as batch_op:
+            batch_op.create_column(sa.Column('id', sa.Integer(), nullable=False))
+            batch_op.create_column(sa.Column('group_code', sa.String(length=50), nullable=True))
+            batch_op.create_column(sa.Column('group_name', sa.String(length=100), nullable=True))
+            batch_op.create_primary_key('pk_groups', ['id'])
 
     # Create the 'students' table
-    if 'students'  not in existing_tables:
-        op.create_table(
-            'students',
-            sa.Column('id', sa.Integer(), nullable=False),
-            sa.Column('name', sa.String(length=100), nullable=True),
-            sa.Column('group_id', sa.Integer(), nullable=True),
-            sa.ForeignKeyConstraint(['group_id'], ['groups.id'], ),
-            sa.PrimaryKeyConstraint('id')
-        )
+    if 'students' not in existing_tables:
+        with op.batch_alter_table('students') as batch_op:
+            batch_op.create_column(sa.Column('id', sa.Integer(), nullable=False))
+            batch_op.create_column(sa.Column('name', sa.String(length=100), nullable=True))
+            batch_op.create_column(sa.Column('group_id', sa.Integer(), nullable=True))
+            batch_op.create_foreign_key('fk_students_group_id_groups', 'groups', ['group_id'], ['id'])
+            batch_op.create_primary_key('pk_students', ['id'])
 
     # Create the 'teachers' table
-    if 'teachers'  not in existing_tables:
-        op.create_table(
-            'teachers',
-            sa.Column('id', sa.Integer(), nullable=False),
-            sa.Column('teacher_name', sa.String(length=100), nullable=True),
-            sa.PrimaryKeyConstraint('id')
-        )
+    if 'teachers' not in existing_tables:
+        with op.batch_alter_table('teachers') as batch_op:
+            batch_op.create_column(sa.Column('id', sa.Integer(), nullable=False))
+            batch_op.create_column(sa.Column('teacher_name', sa.String(length=100), nullable=True))
+            batch_op.create_primary_key('pk_teachers', ['id'])
 
     # Create the 'subjects' table
-    if 'subjects'  not in existing_tables:
-        op.create_table(
-            'subjects',
-            sa.Column('id', sa.Integer(), nullable=False),
-            sa.Column('subj_name', sa.String(length=100), nullable=True),
-            sa.Column('teacher_id', sa.Integer(), nullable=True),
-            sa.ForeignKeyConstraint(['teacher_id'], ['teachers.id'], ),
-            sa.PrimaryKeyConstraint('id')
-        )
+    if 'subjects' not in existing_tables:
+        with op.batch_alter_table('subjects') as batch_op:
+            batch_op.create_column(sa.Column('id', sa.Integer(), nullable=False))
+            batch_op.create_column(sa.Column('subj_name', sa.String(length=100), nullable=True))
+            batch_op.create_column(sa.Column('teacher_id', sa.Integer(), nullable=True))
+            batch_op.create_foreign_key('fk_subjects_teacher_id_teachers', 'teachers', ['teacher_id'], ['id'])
+            batch_op.create_primary_key('pk_subjects', ['id'])
 
     # Create the 'grades' table
-    if 'grades'  not in existing_tables:
-        op.create_table(
-            'grades',
-            sa.Column('id', sa.Integer(), nullable=False),
-            sa.Column('student_id', sa.Integer(), nullable=True),
-            sa.Column('subject_id', sa.Integer(), nullable=True),
-            sa.Column('grade', sa.Integer(), nullable=True),
-            sa.Column('date', sa.Date(), nullable=True),
-            sa.ForeignKeyConstraint(['student_id'], ['students.id'], ),
-            sa.ForeignKeyConstraint(['subject_id'], ['subjects.id'], ),
-            sa.PrimaryKeyConstraint('id')
-        )
+    if 'grades' not in existing_tables:
+        with op.batch_alter_table('grades') as batch_op:
+            batch_op.create_column(sa.Column('id', sa.Integer(), nullable=False))
+            batch_op.create_column(sa.Column('student_id', sa.Integer(), nullable=True))
+            batch_op.create_column(sa.Column('subject_id', sa.Integer(), nullable=True))
+            batch_op.create_column(sa.Column('grade', sa.Integer(), nullable=True))
+            batch_op.create_column(sa.Column('date', sa.Date(), nullable=True))
+            batch_op.create_foreign_key('fk_grades_student_id_students', 'students', ['student_id'], ['id'])
+            batch_op.create_foreign_key('fk_grades_subject_id_subjects', 'subjects', ['subject_id'], ['id'])
+            batch_op.create_primary_key('pk_grades', ['id'])
 
 
 def downgrade() -> None:
     metadata.bind = op.get_bind()
     existing_tables = metadata.tables.keys()
     # Drop the 'grades' table
-    if 'grades'  not in existing_tables:
-        op.drop_table('grades')
+    if 'grades' in existing_tables:
+        with op.batch_alter_table('grades') as batch_op:
+            batch_op.drop_column('date')
+            batch_op.drop_column('grade')
+            batch_op.drop_column('subject_id')
+            batch_op.drop_column('student_id')
+            batch_op.drop_constraint('fk_grades_student_id_students', type_='foreignkey')
+            batch_op.drop_constraint('fk_grades_subject_id_subjects', type_='foreignkey')
+            batch_op.drop_constraint('pk_grades', type_='primary')
 
     # Drop the 'subjects' table
-    if 'subjects'  not in existing_tables:
-        op.drop_table('subjects')
+    if 'subjects' in existing_tables:
+        with op.batch_alter_table('subjects') as batch_op:
+            batch_op.drop_constraint('fk_subjects_teacher_id_teachers', type_='foreignkey')
+            batch_op.drop_column('teacher_id')
+            batch_op.drop_column('subj_name')
+            batch_op.drop_constraint('pk_subjects', type_='primary')
 
     # Drop the 'teachers' table
-    if 'teachers'  not in existing_tables:
-        op.drop_table('teachers')
+    if 'teachers' in existing_tables:
+        with op.batch_alter_table('teachers') as batch_op:
+            batch_op.drop_column('teacher_name')
+            batch_op.drop_constraint('pk_teachers', type_='primary')
 
     # Drop the 'students' table
-    if 'students'  not in existing_tables:
-        op.drop_table('students')
+    if 'students' in existing_tables:
+        with op.batch_alter_table('students') as batch_op:
+            batch_op.drop_constraint('fk_students_group_id_groups', type_='foreignkey')
+            batch_op.drop_column('group_id')
+            batch_op.drop_column('name')
+            batch_op.drop_constraint('pk_students', type_='primary')
 
     # Drop the 'groups' table
-    if 'groups'  not in existing_tables:
-        op.drop_table('groups')
-
+    if 'groups' in existing_tables:
+        with op.batch_alter_table('groups') as batch_op:
+            batch_op.drop_column('group_name')
+            batch_op.drop_column('group_code')
+            batch_op.drop_column('id')
+            batch_op.drop_constraint('pk_groups', type_='primary')
